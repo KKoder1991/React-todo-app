c6478d6c8e426c6880f1bb4e714c4a94099317d8
KKoder1991 <konondlovu@icloud.com>
2021-04-30 16:30:05 +0200

    Add const and functions to add and remove tasks

--- react-todo-app/src/Components/App/App.js
+++ react-todo-app/src/Components/App/App.js
@@ -4,14 +4,35 @@ import TasksList from '../TasksList/TasksList';
 import './App.css';
 
 export default class App extends React.Component{
+  constructor(props){
+    super(props);
+      this.state = {tasks: []}
+
+      this.addTask = this.addTask.bind(this);
+      this.removeTask = this.removeTask.bind(this);
+    }
+
+  addTask(task){
+      this.setState((prevState) => {
+        return {
+          tasks: prevState.tasks.concat(task)
+        };
+      });
+    }
+
+    removeTask(id){
+        const tasks = this.state.tasks.filter(element => (element.id !== id));
+        this.setState({ tasks: tasks });
+      }
+
   render(){
     return (
       <div className="App">
         <header>
-          <h1>Kono's To Do list</h1>
+          <h1>To Do list</h1>
         </header>
-        <SubmitForm />
-        <TasksList />
+        <SubmitForm addTask={this.addTask}/>
+        <TasksList tasks={this.state.tasks} removeTask={this.removeTask} />
       </div>
     );
   }
