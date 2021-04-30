4558e48ddd425c96cb185c99965cb47f7413cefe
KKoder1991 <konondlovu@icloud.com>
2021-04-30 16:32:42 +0200

    map tasks to the task  comp in task list

--- react-todo-app/src/Components/TasksList/TasksList.js
+++ react-todo-app/src/Components/TasksList/TasksList.js
@@ -7,7 +7,13 @@ export default class TasksList extends React.Component{
     render(){
         return(
             <div className="tasks-lists-container">
-                <Task />
+                <div className='tasks-list'>
+                    { this.props.tasks.map(task => 
+                                                        <Task key={task.key}   
+                                                        text={task.text} 
+                                                        id={task.id} 
+                                                        removeTask={this.props.removeTask}/>)}
+                </div>
             </div>
         )
     }
