8affc06f12def9cc96c374a03c74cbef0f64271b
KKoder1991 <konondlovu@icloud.com>
2021-04-30 14:13:59 +0200

    add className to input and button

--- react-todo-app/src/Components/SubmitForm/SubmitForm.js
+++ react-todo-app/src/Components/SubmitForm/SubmitForm.js
@@ -6,8 +6,8 @@ export default class SubmitForm extends React.Component{
         return(
             <div className='submit-form-container'>
                 <form className="submit-form">
-                    <input type="text" placeholder="Add a task"></input>
-                    <button type="submit">Add</button>
+                    <input className="input" type="text" placeholder="Add a task"></input>
+                    <button className="submit-button" type="submit">Add</button>
                 </form>
             </div>
         )
