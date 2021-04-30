1a25fc6b9f8462f119a16254d1a098e25abc48c7
KKoder1991 <konondlovu@icloud.com>
2021-04-30 14:48:02 +0200

    Set properties for Task

--- react-todo-app/src/Components/Task/Task.js
+++ react-todo-app/src/Components/Task/Task.js
@@ -1,19 +1,44 @@
-import './Task.css';
+import "./Task.css";
 
-import React from 'react';
+import React from "react";
 
-export default class Task extends React.Component{
-    render(){
-        return(
-            <div className="task-container">
-                <div className="task-title">
-                    <h2>Task 1</h2>
-                </div>
-                <div className="task-buttons">
-                    <button className="completed-button">+</button>
-                    <button className="delete-button">-</button>
-                </div>
-            </div>
-        )
+export default class Task extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.removeTask = this.removeTask.bind(this);
+    this.markDone = this.markDone.bind(this);
+  }
+
+  removeTask() {
+    this.props.removeTask(this.props.id);
+  }
+
+  markDone() {
+    if (this.state.backgroundColor === " ") {
+      this.setState({ backgroundColor: "rgb(144, 238, 144, 0.5)" });
+    } else {
+      this.setState({ backgroundColor: " " });
     }
+  }
+
+  render() {
+    return (
+      <div className="task-container">
+        <div className="task-container-background" style={{backgroundColor: this.state.backgroundColor}}>
+          <div className="task-title">
+            <h2 className="task-term">{this.props.text}</h2>
+          </div>
+          <div className="task-buttons">
+            <button className="completed-button" onClick={this.markDone}>
+              +
+            </button>
+            <button className="delete-button" onClick={this.removeTask}>
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
