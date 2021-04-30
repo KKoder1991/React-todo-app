0299d46d9af7235c611cec1a6d890d2a112da48f
KKoder1991 <konondlovu@icloud.com>
2021-04-30 16:51:44 +0200

    Add config for firebase to App

--- react-todo-app/src/Components/App/App.js
+++ react-todo-app/src/Components/App/App.js
@@ -3,7 +3,18 @@ import SubmitForm from '../SubmitForm/SubmitForm';
 import TasksList from '../TasksList/TasksList';
 import './App.css';
 
+const config = {
+  apiKey: "AIzaSyCwAc4Fi4ghor7hyaf5YWjWsWKV2wSBnPc",
+  authDomain: "to-do-list1991.firebaseapp.com",
+  projectId: "to-do-list1991",
+  storageBucket: "to-do-list1991.appspot.com",
+  messagingSenderId: "462733111573",
+  appId: "1:462733111573:web:ae1fd06f020330db9b791c",
+  measurementId: "G-KBBY27D4Q3"
+}
+
 export default class App extends React.Component{
+
   constructor(props){
     super(props);
       this.state = {tasks: []}
