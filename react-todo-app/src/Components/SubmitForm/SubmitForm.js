45c10de11a508b5a6ef575a7478f419da798f7ad
KKoder1991 <konondlovu@icloud.com>
2021-04-30 14:48:52 +0200

    Create add item function to add new task

--- react-todo-app/src/Components/SubmitForm/SubmitForm.js
+++ react-todo-app/src/Components/SubmitForm/SubmitForm.js
@@ -2,6 +2,31 @@ import React from 'react';
 import './SubmitForm.css';
 
 export default class SubmitForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = { tasksCount: 0};
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e){
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                text: this._inputElement.value,
+                id: this.state.tasksCount,
+                key: Date.now()
+            };
+
+            this.props.addTask(newItem);
+
+            this.state.tasksCount++;
+            this._inputElement.value = "";
+        }
+
+        e.preventDefault();
+    }
+
     render(){
         return(
             <div className='submit-form-container'>
